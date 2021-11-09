56754db45e698d7d11b2ea0237677b053e140eea
alicepetzinger <alice.petzinger@gmail.com>
2021-11-09 15:58:45 +0100

    finalize styleguide

--- src/Components/ImageInput/ImageInput.stories.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ImageInput from './ImageInput';
-
-export default {
-  component: ImageInput,
-  title: 'ImageInput',
-};
-
-export const Default = () => <ImageInput />;
