15bb2c57eeda9f356c621cff199a405207297124
alicepetzinger <alice.petzinger@gmail.com>
2021-11-09 21:12:07 +0100

    💄 Add Image Input

---
+++ src/app/Components/ImageInput/ImageInput.tsx
@@ -0,0 +1,30 @@
+import React, { ChangeEvent, useState } from 'react';
+import ScanIcon from './ScanIcon';
+import styles from './ImageInput.module.css';
+
+function ImageInput() {
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      return;
+    }
+    const file = event.target.files[0];
+    const newImageURL = URL.createObjectURL(file);
+    setImageUrl(newImageURL);
+  };
+
+  return (
+    <label className="scanIcon">
+      <label htmlFor="Start" className={styles.button}>
+        Import
+      </label>
+      <ScanIcon />
+      <input id="Start" type="file" accept="image/*" onChange={handleChange} />
+
+      {imageUrl && <img className={styles.img} src={imageUrl} />}
+    </label>
+  );
+}
+
+export default ImageInput;
